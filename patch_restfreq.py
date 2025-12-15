627b19c4724bcfe9c551b3c89524d7ba25af1cac
Peter Teuben <teuben@gmail.com>
2025-12-15 13:59:51 -0500

    properly done now, do NOT rely on B having always ifnum=1

--- patch_restfreq.py
+++ patch_restfreq.py
@@ -2,31 +2,28 @@
 #
 #  In https://github.com/GreenBankObservatory/dysh/issues/682 I mentioned
 #  it was complicated (at the time) to patch the RESTFREQ. This script
-#  bypasses that for the 2015 data, but only for ifnum=3, thus also cutting
+#  bypasses that for the 2015 data, but only for ifnum=1, thus also cutting
 #  I/O by factor 3.
 #
 #  on lma for 39 conversions:  28.85user 97.22system 2:57.31elapsed 71%CPU
 #  on d76 for  1 conversion:    4.61user  9.54system 0:12.85elapsed 110%CPU  (via dysh)
 #                               3.81user  3.07system 0:05.44elapsed 126%CPU  (via python)
-#  Typical usage:
+#  Usage:
 #
-#   ./patch_restfreq.py /lma2/teuben/dysh_data/sdfits/AGBT15B_287/AGBT15B_287_??.raw.vegas/AGBT15B_287_??.raw.vegas.B.fits
+#   ./patch_restfreq.py
 #
-#  i.e. it needs a full path, since SDFITSLoad doesn't know about $SDFITS_PATH yet
 #
+
 import os
 import sys
 import numpy as np
 
-from dysh.fits.sdfitsload import SDFITSLoad
+from dysh.fits.gbtfitsload import GBTOffline
 
-for f in sys.argv[1:]:
-    print(f"Working on {f}")   # AGBT15B_287_19.raw.vegas.B.fits
-    head, tail = os.path.split(f)
-    session = tail[12:14]
-    out = tail[:14] + ".B.fits"
-    print(f"Writing {out}")
-    sdf = SDFITSLoad(f)
-    sdf._bintable[0].data["RESTFREQ"] = 1420.405751786e6
-    sdf.write(out, overwrite=True) 
-
+for s in [1,3,4,5,6,8,9] + list(range(10,42)):
+    fni = f"AGBT15B_287_{s:02}"
+    fno = f"data/AGBT15B_287_{s:02}.fits"
+    print(fni,fno)
+    sdf = GBTOffline(fni)
+    # sdf['RESTFREQ'] = 1420405751.786
+    sdf.write(fno, ifnum=1, overwrite=True, multifile=False)
